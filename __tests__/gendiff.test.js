72ad80471ff5b7bb349ec274cd18efbb060a8c8b
Ian Proletov <yan.proletov@gmail.com>
2019-01-24 18:45:43 +0300

    test duplication code correct

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,16 +1,22 @@
 import fs from 'fs';
+import path from 'path';
 import genDiff from '../src';
 
 describe('verify the result', () => {
   const pathOfExpected = '__tests__/__fixtures__/res';
+  const expected = fs.readFileSync(pathOfExpected, 'utf-8');
+  const pathOfFixtures = '__tests__/__fixtures__';
+  const actualMake = (before, after) => {
+    const beforePath = path.join(pathOfFixtures, before);
+    const afterPath = path.join(pathOfFixtures, after);
+    return genDiff(beforePath, afterPath);
+  };
   it('JSON check', () => {
-    const actual = genDiff('__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.json');
-    const expected = fs.readFileSync(pathOfExpected, 'utf-8');
+    const actual = actualMake('before.json', 'after.json');
     expect(actual).toBe(expected);
   });
   it('YAML check', () => {
-    const actual = genDiff('__tests__/__fixtures__/before.yml', '__tests__/__fixtures__/after.yml');
-    const expected = fs.readFileSync(pathOfExpected, 'utf-8');
+    const actual = actualMake('before.yml', 'after.yml');
     expect(actual).toBe(expected);
   });
 });
