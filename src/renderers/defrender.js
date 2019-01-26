ee915d95b5ff35e35728fd1b475622ff8b169e06
Ian Proletov <yan.proletov@gmail.com>
2019-01-26 22:19:21 +0300

    stringify upgrade

--- src/renderers/defrender.js
+++ src/renderers/defrender.js
@@ -6,14 +6,16 @@ const chartype = {
 };
 
 const stringify = (content, deepSize) => {
-  const preident = ' '.repeat(deepSize * 4 + 2);
-  const postident = ' '.repeat(deepSize * 4);
-  const result = Object.keys(content).map(key => (`${preident}  ${key}: ${content[key]}`));
-  return `{\n${result.join('\n')}\n${postident}}`;
+  if (content instanceof Object) {
+    const preident = ' '.repeat(deepSize * 4 + 2);
+    const postident = ' '.repeat(deepSize * 4);
+    const result = Object.keys(content).map(key => (`${preident}  ${key}: ${content[key]}`));
+    return `{\n${result.join('\n')}\n${postident}}`;
+  }
+  return content;
 };
 
 const render = (abstract, deepSize = 0) => {
-  const currentValue = value => (value instanceof Object ? stringify(value, deepSize + 1) : value);
   const preident = ' '.repeat(deepSize * 4 + 2);
   const postident = ' '.repeat(deepSize * 4);
   const result = abstract.map((element) => {
@@ -22,11 +24,11 @@ const render = (abstract, deepSize = 0) => {
       return `${preident}${chartype[type]} ${key}: ${render(element.children, deepSize + 1)}`;
     }
     if (type === 'updated') {
-      const added = `${preident}${chartype.added} ${key}: ${currentValue(element.nextValue)}`;
-      const removed = `${preident}${chartype.removed} ${key}: ${currentValue(element.prevValue)}`;
+      const added = `${preident}${chartype.added} ${key}: ${stringify(element.nextValue, deepSize + 1)}`;
+      const removed = `${preident}${chartype.removed} ${key}: ${stringify(element.prevValue, deepSize + 1)}`;
       return [added, removed].join('\n');
     }
-    return `${preident}${chartype[type]} ${key}: ${currentValue(value)}`;
+    return `${preident}${chartype[type]} ${key}: ${stringify(value, deepSize + 1)}`;
   });
   return `{\n${result.join('\n')}\n${postident}}`;
 };
