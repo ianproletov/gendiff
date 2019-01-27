420f682289623877c4e04c86332c2a261d27b3a3
Ian Proletov <yan.proletov@gmail.com>
2019-01-27 19:54:51 +0300

    json render refactor

--- src/renderers/jsonrender.js
+++ src/renderers/jsonrender.js
@@ -1,28 +1,3 @@
-const stringify = (content) => {
-  if (content instanceof Object) {
-    const result = Object.keys(content).map(key => (`"${key}":"${content[key]}"`));
-    return `{${result.join(',')}}`;
-  }
-  return `"${content}"`;
-};
-
-const jsonRender = (abstract) => {
-  const result = abstract.map((element) => {
-    const { key, value, type } = element;
-    switch (type) {
-      case 'samedeep':
-        return (`"${key}":${jsonRender(element.children)}`);
-      case 'updated':
-        return `"${key}":{"newValue":${stringify(element.nextValue)},"oldvalue":${stringify(element.prevValue)},"type":"${type}"}`;
-      case 'added':
-      case 'removed':
-      case 'same':
-        return `"${key}":{"value":${stringify(value)},"type":"${type}"}`;
-      default:
-    }
-    return null;
-  });
-  return `{${result.join(',')}}`;
-};
+const jsonRender = abstract => JSON.stringify(abstract);
 
 export default jsonRender;
